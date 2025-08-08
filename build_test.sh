03159bd751fd60f67a71eac77498fc3f62e8cd54
levijcdf <levijcfigueiredowebdev@gmail.com>
2025-08-08 14:27:44 +0100

    Update bash script

--- build_test.sh
+++ build_test.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+gcc main.c -o build/test
 
-gcc main.c -o build/test;
-./build/test;
+echo "Do you want to run the program? (Y/N)"
+read -r ans
+
+# Convertir a minúsculas para que acepte Y o y
+ans=${ans,,}
+
+if [[ "$ans" == "y" ]]; then
+    ./build/test
+else
+    echo "Programa no ejecutado."
+fi
